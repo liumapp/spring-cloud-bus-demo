e402e003af344dd6ed2605043077066502440dcb
liumapp <liumapp.com@gmail.com>
2018-03-22 14:22:06 +0800

    add actuator dependency

--- bus-service/demo-api-a/src/main/java/com/liumapp/demo/bus/service/demoapia/controller/IndexController.java
+++ bus-service/demo-api-a/src/main/java/com/liumapp/demo/bus/service/demoapia/controller/IndexController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
  * @author liumapp
  * @file IndexController.java
